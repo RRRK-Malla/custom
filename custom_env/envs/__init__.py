a5447e43a516d623d2ddaeaa674e87cb8e399a90
RRRK-Malla <81765039+RRRK-Malla@users.noreply.github.com>
2021-06-03 01:09:27 +1000

    Update __init__.py

--- custom_env/envs/__init__.py
+++ custom_env/envs/__init__.py
@@ -1,6 +1,1 @@
-register(
-    id='Drivey-v1',
-    entry_point='custom_envs.envs:DtestEnv',
-    reward_threshold=8, # optimum = 8.46
-    max_episode_steps=200,
-)
+from custom_drive.envs.taxi import DtestEnv
