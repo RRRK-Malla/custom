b3d6539f34d6f57f3ce0e92c112fd32906f2608c
RRRK-Malla <81765039+RRRK-Malla@users.noreply.github.com>
2021-06-03 01:10:09 +1000

    Update __init__.py

--- custom_env/__init__.py
+++ custom_env/__init__.py
@@ -1,6 +1,8 @@
 from gym.envs.registration import register
 
 register(
-    id='Drivey-v0',
-    entry_point='custom_env.envs:DtestEnv',
+    id='Drivey-v1',
+    entry_point='custom_envs.envs:DtestEnv',
+    reward_threshold=8, # optimum = 8.46
+    max_episode_steps=200,
 )
